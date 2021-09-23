dbcfa2a8a4be05a0839424e84d047c03f1b10aaa
shwetaevidev <shweta@evidev.tech>
2021-09-23 19:20:24 +0530

    api-test-framework set up

---
+++ src/main/java/ControllerUtils/LoginControllerUtils.java
@@ -0,0 +1,24 @@
+package ControllerUtils;
+
+
+
+import io.restassured.response.Response;
+import pojo.Token;
+import utils.Configuration;
+import utils.RestAssuredUtil;
+
+
+public class LoginControllerUtils extends RestAssuredUtil {
+
+    public static final String BASE_URI = "https://connect.routee.net";
+    public static final String SMS_URI = "/sms";
+    public static Token token= Configuration.routeeAuth0Login();
+
+
+
+    public static Response postSMS(String payload) {
+        return RestAssuredUtil.post(BASE_URI, SMS_URI, payload,token.getAccess_token());
+    }
+
+
+}
